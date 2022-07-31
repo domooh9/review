2008f9a032341043e0e8bd781a297c1a08a19a16
Domnic Omondi <domooh9@gmail.com>
2022-07-31 17:48:43 +0300

    Loader components created

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className='loader flex flex-c'>
+      <h3>Loading...</h3>
+    </div>
+  )
+}
+
+export default Loader
